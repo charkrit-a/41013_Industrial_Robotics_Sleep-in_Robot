0a7827fa362c64741c04c94c562f84a64c768a4f
charkrit-a <charkritatherton@gmail.com>
2024-10-23 08:58:30 +1100

    cleaning A2 after merge

--- A2.m
+++ A2.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function A2(app)
     clf
     clc
@@ -13,14 +12,3 @@ function A2(app)
         pause(0.01);
     end
 end
-=======
-clear
-clf
-clc
-
-Init();
-
-hold on
-
-EnvironmentA2();
->>>>>>> 081c179c01975d1a3b5537c778500d0181387a99
